df9e871930dc0b4637198ccf6b70957cc07f87f4
MattClarke246 <clarke.matthew95@gmail.com>
2026-09-16 00:39:18 -0500

    Wire automatic logSession trigger on app launch for return-user telemetry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
+import React, { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import * as Notifications from 'expo-notifications';
 import { AuthReadyProvider } from '../context/AuthReadyContext';
 import { StatsProvider } from '../context/StatsContext';
 import { ThemeProvider } from '../context/ThemeContext';
+import { logSession } from '../utils/analytics';
 
 Notifications.setNotificationHandler({
     handleNotification: async () => ({
@@ -15,6 +17,11 @@ Notifications.setNotificationHandler({
 });
 
 export default function RootLayout() {
+    useEffect(() => {
+        // Fire-and-forget anonymous session tracking for return-user impact reporting
+        logSession();
+    }, []);
+
     return (
         <AuthReadyProvider>
             <StatsProvider>
